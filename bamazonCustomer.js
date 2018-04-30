2f4287e4967b878011d0869224b54b822496dcd9
Sarah Shelden <sarah.shelden113@gmail.com>
2018-04-30 15:03:08 -0400

    bamazon customer

---
+++ bamazonCustomer.js
@@ -0,0 +1,69 @@
+// requiring
+
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+let Table = require('cli-table');
+
+// sql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'MyNewPass',
+    database: 'bamazon'
+});
+
+// error
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected as id' + connection.threadId);
+    startBuying();
+});
+
+
+function printStuff (res) {
+    const table = new Table({
+        head: ['Item Id', 'Product Name', 'Department', 'cost', 'stock'],
+        colWidths: [10, 45,40, 8,8]
+    });
+    for (const i =0; i < res.length; i++) {
+        table.push([res[i].itemID,res[i].product_name, res[i].department_name, res[i].item_cost, res[i].stock_quanity]);
+    }
+     console.log(table.toString());
+}
+
+var startBuying = function() {
+    connection.query('SELECT * FROM products', function(err, res) {
+        printStuff(res);
+        var choiceArray = [];
+        for (var i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].product_name);
+        }
+
+        inquirer.prompt([{
+            name: "item",
+            type: "input",
+            message: 'Which item would you like to purchase? (Enter item ID)'
+        },
+            { name:'quantity',
+              type: 'input',
+              message: "How many would you like to purchase?"
+
+            }]).then(function(answer) {
+                console.log(answer);
+                var itemID = answer.item;
+                console.log(itemID);
+                var chosenItem = res[itemID-1];
+                console.log(chosenItem);
+                var newQuantity = chosenItem.stock_quantity - answer.quantity;
+                if(newQuantity >= 0){
+                    connection.query("UPDATE products SET ? WHERE itemID = ?",[{stock_quantity: newQuantity}, itemID]);
+                    startBuying();
+                } else {
+                    console.log('there are not enough in stock for you to purchase that many items.');
+                }
+            })
+    })
+
+
+    };
